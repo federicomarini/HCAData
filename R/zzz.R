3906832932a0d05746079490a0b507a9c0d34e35
Federico Marini <marinif@uni-mainz.de>
2018-11-05 23:14:02 +0100

    updated imports and namespace entries

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,8 @@
 #' @importFrom utils read.csv
-#' @importFrom ExperimentHub createHubAccessors
+#' @importFrom ExperimentHub createHubAccessors ExperimentHub
+#' @importFrom AnnotationHub query
+#' @importFrom HDF5Array HDF5Array
+#' @importFrom SingleCellExperiment SingleCellExperiment
 .onLoad <- function(libname, pkgname) {
   fl <- system.file("extdata", "metadata.csv", package="HCAData")
   titles <- read.csv(fl, stringsAsFactors=FALSE)$Title
